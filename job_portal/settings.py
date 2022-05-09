35fc41689efb6c9c19cf832a31d3d8a5251998ea
namansingh3502 <namansingh0305@gmail.com>
2022-05-09 21:08:59 +0530

    enabled logs

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -104,53 +104,53 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'verbose': {
-#             'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-#             'datefmt': "%d/%b/%Y %H:%M:%S"
-#         },
-#         'simple': {
-#             'format': '%(levelname)s %(message)s'
-#         },
-#     },
-#     'handlers': {
-#         'null': {
-#             'level': 'DEBUG',
-#             'class': 'logging.NullHandler',
-#         },
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'verbose'
-#         },
-#         'logfile': {
-#             'level': 'DEBUG',
-#             'class': 'logging.handlers.WatchedFileHandler',
-#             'filename': "/home/naman/Desktop/job_portal/logs/gunicorn_log/forum.log",
-#             'formatter': 'verbose',
-#         },
-#     },
-#     'loggers': {
-#         'django.request': {
-#             'handlers': ['logfile'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#         'django': {
-#             'handlers': ['logfile'],
-#             'propagate': True,
-#             'level': 'DEBUG',
-#         },
-#         'job_portal': {
-#             'handlers': ['console', 'logfile'],
-#             'level': 'DEBUG',
-#             'propagate': False,
-#         },
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': "/home/naman/Desktop/job_portal/logs/gunicorn_log/forum.log",
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['logfile'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['logfile'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'job_portal': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
